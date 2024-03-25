1ce62d3e068d1c10f013fd04ff3c91860ef82ef4
Ansuman Jenamani <anshumanjenamani@gmail.com>
2024-03-26 01:39:13 +0530

    added new files and updated

--- basicCode.java
+++ basicCode.java
@@ -4,3 +4,5 @@ a=20;
 b=20;
 sum=a+b
 System.out.println(sum);
+System.out.println(a);
+System.out.println(b);
---
+++ diff.java
@@ -0,0 +1,10 @@
+class generic{
+
+	public static void main(String [] args){
+
+	int x= 20;
+	int y = 10;
+	int sum=0;
+	sum = x-y
+		System.out.println(sum);
+}
